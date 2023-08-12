cd8f81bee5d06d9c4304d5c013be3790328bf56c
Breno Cardoso <elisiobreno@gmail.com>
2023-08-12 00:22:13 -0300

    Middleware configurations on Chi Router

--- server/router.go
+++ server/router.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"time"
+
 	"github.com/go-chi/chi/v5"
 )
 
@@ -8,9 +10,11 @@ var (
 	V1 = "/api/v1/"
 )
 
-func GetRoutes() *chi.Mux {
-
+func GetRoutes(configs *config.Config) *chi.Mux {
 	router := chi.NewRouter()
+
+	router.Use(middleware.Logger)
+	router.Use(middleware.Timeout(60 * time.Second))
 
 	return router
 }
